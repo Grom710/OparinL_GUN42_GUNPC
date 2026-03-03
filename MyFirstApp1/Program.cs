f983a0e7351e87dbffbc3cd8631ae80a688c4e58
Grom710 <levbearp1@gmail.com>
2026-03-03 13:28:24 +0000

    Добавьте файлы проекта.

---
+++ MyFirstApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Netologia 1000");
+{
+    if (!Int32.TryParse(Console.ReadLine(), out var a))
+    {
+        Console.WriteLine("Not a number!");
+        return;
+    }
+
+    if (!Int32.TryParse(Console.ReadLine(), out var b))
+    {
+        Console.WriteLine("Not a number");
+        return;
+    }
+
+    var s = (Console.ReadLine());
+    var boolVar = true;
+    if (s.Length == 0 || s.Length > 1 && !boolVar)
+    {
+        Console.WriteLine("Wrong sign");
+        return;
+    }
+
+    switch (s[0])
+    {
+        case '+':
+            Console.WriteLine("Result of {0} + {1} = {2}", a, b, a + b);
+            break;
+        case '-':
+            Console.WriteLine("Result of {0} + {1} = {2}", a, b, a - b);
+            break;
+        case '*':
+            Console.WriteLine("Result of {0} * {1} = {2}", a, b, a * b);
+            break;
+        case '/':
+            Console.WriteLine("Result of {0} / {1} = {2}", a, b, a / b);
+            break;
+        case '%':
+            Console.WriteLine("Result of {0} % {1} = {2}", a, b, a % b);
+            break;
+        default:
+            Console.WriteLine("Wrong sign");
+            break;
+
+    }
+}
+
+    
+
+
