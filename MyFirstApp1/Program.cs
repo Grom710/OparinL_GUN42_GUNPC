65e838a8e2f7b449cd0acd484b544bdf9d67493a
Grom710 <levbearp1@gmail.com>
2026-03-05 19:51:22 +0000

    CYCLE

--- MyFirstApp1/Program.cs
+++ MyFirstApp1/Program.cs
@@ -1,4 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
 using System.Net.NetworkInformation;
 
-int[] array1 = new int[] { 32, 111, 25, 17 }; 
+int[] array1 = new int[] { 32, 111, 25, 17 };
+
+
+for (int index = array1.Length - 1; index >= 0; index--)
+    if (array1[index] == 111)
+{
+    continue
+}
+Console.WriteLine(array1[index]);
+
+
+
+//int value = 150;
+//int result = 0;
+//int index = 0;
+//do 
+//{
+//result += array1[index];
+//} while (result < value);
+//Console.WriteLine(result.ToString());
+
+//while (index < array1.Length   ) 
+//{
+//    if (array1[index] >= 111)
+//    {
+//        index++;
+//        continue;
+//    }
+//    Console.WriteLine( array1[index] );
+//    index++;
