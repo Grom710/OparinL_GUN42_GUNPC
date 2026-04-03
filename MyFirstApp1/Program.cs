32f11a4ae82ce62a9a749e4e51eba2284f149dca
Grom710 <levbearp1@gmail.com>
2026-04-03 15:07:23 +0000

    Task 1

--- MyFirstApp1/Program.cs
+++ MyFirstApp1/Program.cs
@@ -1,3 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//using System;
+//using System.Net.NetworkInformation;
 
+//class PROGRAM
+//{
+//    static void Main()
+//    {
+//        int n = 10;
+//        int a = 0, b = 1;
+//        int count = 0;
+//        Console.WriteLine("First numbers to Fb");
+//        while (count < n)
+//        {
+//            Console.WriteLine(a + " ");
+//            int next = a + b;
+//            a = b;
+//            b = next;
+//            count++;
+//        }
+
+    
+//    }
+//}
 
